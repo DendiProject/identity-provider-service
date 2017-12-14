76c16d8f2f7a93596f6d949ca5de7357fc6e6fbb
Artem Shevelyuhin <artem_16_97@mail.ru>
2017-12-14 22:19:54 +0300

    Rename packages. Change pom file

--- src/main/java/springboot/test/TestApplication.java
+++ src/main/java/springboot/test/TestApplication.java
@@ -1,15 +1,14 @@
-package SpringBoot.test;
+package springboot.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.joda.time.LocalTime;
+
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-                LocalTime currentTime = new LocalTime();
-		System.out.println("The current local time is: " + currentTime);
+               
                 Greeter greeter = new Greeter();
                 System.out.println(greeter.sayHello());
         
--- src/test/java/SpringBoot/test/TestApplicationTests.java
+++ src/test/java/SpringBoot/test/TestApplicationTests.java
@@ -1,24 +1,20 @@
-package SpringBoot.test;
+package springboot.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
 
-        private Greeter greeter = new Greeter();
+       
 	@Test
 	public void contextLoads() {
 	}
-        @Test
-	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("Hello"));
-	}
+       
 
 }

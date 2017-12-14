c88d3b9c1a59758a09d4ef9560502ccc6173187b
Artem Shevelyuhin <artem_16_97@mail.ru>
2017-12-14 16:53:54 +0300

    add helloworld

--- src/main/java/SpringBoot/test/TestApplication.java
+++ src/main/java/SpringBoot/test/TestApplication.java
@@ -2,14 +2,17 @@ package SpringBoot.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.joda.time.LocalTime;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++");
-                System.out.println("Hello Wotld !!!");
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+                LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+                Greeter greeter = new Greeter();
+                System.out.println(greeter.sayHello());
+        
 		SpringApplication.run(TestApplication.class, args);
 	}
 }
--- src/test/java/SpringBoot/test/TestApplicationTests.java
+++ src/test/java/SpringBoot/test/TestApplicationTests.java
@@ -5,12 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestApplicationTests {
 
+        private Greeter greeter = new Greeter();
 	@Test
 	public void contextLoads() {
+	}
+        @Test
+	public void greeterSaysHello() {
+		assertThat(greeter.sayHello(), containsString("Hello"));
 	}
 
 }

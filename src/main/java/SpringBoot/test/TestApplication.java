093b183f51c47f6aa91f8e619f643917990a6a2e
Artem Shevelyuhin <artem_16_97@mail.ru>
2017-12-14 16:35:01 +0300

    hello add

--- src/main/java/SpringBoot/test/TestApplication.java
+++ src/main/java/SpringBoot/test/TestApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("Hello Wotld !!!");
+                System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 		SpringApplication.run(TestApplication.class, args);
 	}
 }

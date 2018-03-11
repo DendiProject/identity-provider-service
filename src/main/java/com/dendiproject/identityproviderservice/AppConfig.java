a095e3698e10cb1cdc3941da1a6a72e04861cf31
Artem Shevelyuhin <artem_16_97@mail.ru>
2018-03-11 13:17:21 +0300

    app config changes

---
+++ src/main/java/com/dendiproject/identityproviderservice/AppConfig.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dendiproject.identityproviderservice;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+/**
+ *
+ * @author ArtemShevelyukhin
+ */
+@Configuration
+public class AppConfig {
+    
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    
+   
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+    
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+    
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        
+        //dataSource.setDriverClassName(dbDriverClassName);
+        dataSource.setUrl(datasourceUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        
+        return dataSource;
+    }
+    
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource());
+    }
+}

4da0df4a30921c0cf3ac785f675bae2b28e57ee9
lizagorovets <liza.gorovets@yandex.ru>
2018-05-20 15:34:19 +0300

    Add request to get users

---
+++ src/main/java/com/dendiproject/identityproviderservice/model/UserInfoDto.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dendiproject.identityproviderservice.model;
+
+/**
+ *
+ * @author eliza
+ */
+public class UserInfoDto {
+    private String id;
+  private String name;
+  private String lastname;
+  private String displayname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    
+}
--- src/main/java/com/dendiproject/identityproviderservice/service/UserServiceImpl.java
+++ src/main/java/com/dendiproject/identityproviderservice/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.dendiproject.identityproviderservice.service;
 
+import com.dendiproject.identityproviderservice.conversion.Conversion;
 import com.dendiproject.identityproviderservice.model.User;
+import com.dendiproject.identityproviderservice.model.UserInfoDto;
 import com.dendiproject.identityproviderservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,6 +18,8 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserRepository userRepository;
+    @Autowired
+  private Conversion convertor;
 
   @Override
   @Transactional
@@ -25,6 +31,24 @@ public class UserServiceImpl implements UserService {
   public User findByUsername(String name) {
     return userRepository.getOne(name);
   }
+  
+  @Override
+    public List<UserInfoDto> getAll() {
+        List<User> users=userRepository.findAll();
+        return users.stream()
+               .map(user->convertor.convertUserToUserInfoDtoDto(user))
+               .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserInfoDto getById(String userId) {
+        User user=userRepository.findById(userId);
+        if (user!=null)
+        {
+            return convertor.convertUserToUserInfoDtoDto(user);
+        }
+        return null;
+    }
 
 //    @Override
 //    public void updateUserDetails(User user) {
